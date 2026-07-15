47f0c20e8f9628fc7f3b2c78735bc90c7aa34eb1
Malamgie <95567029+Malamgie@users.noreply.github.com>
2026-07-15 08:48:08 +0100

    Remove Firebase config values from firebase.js
    Removed sensitive Firebase configuration details.

--- client/js/firebase.js
+++ client/js/firebase.js
@@ -18,12 +18,7 @@ import {
 // =====================================
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnnAJXnXxHcSBfD46jmr0KGSR_KstOw9s",
-  authDomain: "metaescrow-c10f3.firebaseapp.com",
-  projectId: "metaescrow-c10f3",
-  storageBucket: "metaescrow-c10f3.firebasestorage.app",
-  messagingSenderId: "942736759161",
-  appId: "1:942736759161:web:952a4df03d2958fdb5b7a8"
+  
 };
 
 
